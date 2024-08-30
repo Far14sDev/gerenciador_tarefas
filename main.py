75334b42583c0abe1341a64d219b62250b18cb1e
Francisco Farias Neto <francisco.fariasneto14@gmail.com>
2024-08-30 18:46:59 -0300

    adicionado código para listar tarefas

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ def adicionar_tarefa(lista_tarefas, tarefa):
     """Adiciona uma nova tarefa à lista de tarefas."""
     # Implementar a função aqui
 
-def listar_tarefas(lista_tarefas):
-    """Lista todas as tarefas com seus índices."""
-    # Implementar a função aqui
+def listar_tarefas ():
+    print("Selecione o tipo de listagem:\n")
+    print("|=========================================|")
+    print("| 1 - Listar todas as tarefas             |")
+    print("| 2 - Listar tarefas por prioridade       |")
+    print("| 3 - Listar tarefas por categoria        |")
+    print("| 4 - Sair                                |")
+    print("|=========================================|")
+    opc = int(input(": -> "))
+    if opc == 1:
+        for t in tarefas:
+            print(t)
+    elif opc == 2:
+        select = int(input("Digite a prioridade: "))
+        for i in tarefas:
+            if i["prioridade"] == select:
+                print(f"Tarefa: {i["nome"]}, Prioridade: {i["prioridade"]}")
+    elif opc == 3:
+        select = int(input("Digite a categoria: "))
+        for i in tarefas:
+            if i["categoria"] == select:
+                print(f"Tarefa: {i["nome"]}, Categoria: {i["categoria"]}")
+    elif opc == 4:
+        return menu
+
 
 def remover_tarefa(lista_tarefas, indice):
     """Remove uma tarefa da lista pelo índice."""
